cc6557b2cd874c6acdcc6e3f3bc0c5434060b06a
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-02-02 14:13:44 -0700

    Fix interactions

--- rockpaperscissors/rock.js
+++ rockpaperscissors/rock.js
@@ -1,22 +1,42 @@
 let humanScore = 0;
 let computerScore = 0;
+const score = document.querySelector(".score");
+score.setAttribute("style", "font-size: 30px");
+const ROCK = document.querySelector("img.rock");
+const PAPER = document.querySelector("img.paper");
+const SCISSORS = document.querySelector("img.scissor");
+const result = document.querySelector(".result");
 
 function playRound(humanChoice, computerChoice) {
-    const result = document.querySelector(".results");
-    result.setAttribute("style", "font-size: 30px");
     if (humanChoice == computerChoice) {
-        result.textContent =  `Draw!\r\n ${humanChoice} is equal ${computerChoice}`
+        result.textContent =  `Draw!\r\n Your choice: ${humanChoice}, is the same to ${computerChoice}`
+        score.textContent =  `Human: ${humanScore} vs Computer: ${computerScore}`
         return;
     }
     if ((humanChoice == "ROCK" && computerChoice == "SCISSORS") ||
         (humanChoice == "PAPER" && computerChoice == "ROCK") || 
         (humanChoice == "SCISSORS" && computerChoice == "PAPER")) {
             result.textContent =  `You Win!\r\n ${humanChoice} beats ${computerChoice}`
-            humanScore++    
+            humanScore++;
         } else {
-            computerScore++
+            computerScore++;
             result.textContent =  `You Lose!\r\n ${humanChoice} beats ${computerChoice}`
         }
+    score.textContent =  `Human: ${humanScore} vs Computer: ${computerScore}`
+}
+
+function checkScore() {
+    if (humanScore < 5 && computerScore < 5) {
+        console.log(`Human ${humanScore} vs Computer ${computerScore}`)
+        return true;
+    } else {
+        if ( humanScore > computerScore) {
+            score.textContent =  `GAME OVER! You win. Human: ${humanScore} vs Computer: ${computerScore}`
+        } else {
+            score.textContent =  `GAME OVER! Computer Wins. Human: ${humanScore} vs Computer: ${computerScore}`
+        }
+        return false;
+    }
 }
 
 const getComputerChoice = () => {
@@ -30,22 +50,29 @@ const getComputerChoice = () => {
     }
 }
 
-const ROCK = document.querySelector("img.rock");
-const PAPER = document.querySelector("img.paper");
-const SCISSORS = document.querySelector("img.scissor");
-
-
-console.log(`Human ${humanScore} vs Computer ${computerScore}`)
 
 ROCK.addEventListener("click", () => {
+    if (!checkScore()) {
+        return;
+    }
     playRound("ROCK", getComputerChoice())
 });
-ROCK.addEventListener("hover", () => {});
 
 PAPER.addEventListener("click", () => {
+    if (!checkScore()) {
+        return;
+    }
     playRound("PAPER", getComputerChoice())
 });
+
 SCISSORS.addEventListener("click", () => {
+    if (!checkScore()) {
+        return;
+    }
     playRound("SCISSORS", getComputerChoice())
 });
+
+
+
+
 
