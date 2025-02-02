9ae3872d6c45414035c1adb334acbd529c9f37b6
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-02-02 16:22:58 -0700

    Refactor and functionality
    Changed some variables. You can now set the size of squares. Erase the
    content of them.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,22 +1,41 @@
-const grid_size = 16;
 const container = document.querySelector(".container");
-const square_height = container.clientHeight / grid_size;
-const square_width = container.clientWidth / grid_size;
+const input_button = document.querySelector(".input-button");
 const erase_button = document.querySelector(".erase-button");
+let grid_size = 16;
+let square_height = container.clientHeight / grid_size;
+let square_width = container.clientWidth / grid_size;
+
+let grid = createGrid(grid_size);
+
+input_button.addEventListener("click", () =>{
+    grid_size = prompt("Input grid size(max size=100):");
+    if (grid_size > 100) {
+        grid_size = 100;
+    }
+    square_height = container.clientHeight / grid_size;
+    square_width = container.clientWidth / grid_size;
+    console.log(grid_size);
+    grid = createGrid(grid_size);
+});
 
 
-for (let i = 0; i < grid_size; i++) {
-    for (let j = 0; j < grid_size; j++) {
-        const element = document.createElement("div");
-        element.style.width = `${square_height}px`;
-        element.style.height =`${square_width}px`;
-        element.style.backgroundColor = "white";
-        element.addEventListener("mouseover", () => {
-            element.style.backgroundColor = "red";
-        });
-        erase_button.addEventListener("click", () => {
+function createGrid(grid_size) {
+    for (let i = 0; i < grid_size; i++) {
+        for (let j = 0; j < grid_size; j++) {
+            const element = document.createElement("div");
+            element.style.width = `${square_height}px`;
+            element.style.height =`${square_width}px`;
             element.style.backgroundColor = "white";
-        });
-        container.appendChild(element);
+            element.addEventListener("mouseover", () => {
+                element.style.backgroundColor = "red";
+            });
+            erase_button.addEventListener("click", () => {
+                element.style.backgroundColor = "white";
+            });
+            input_button.addEventListener("click", () =>{
+                element.remove();
+            });
+            container.appendChild(element);
+        }
     }
 }
