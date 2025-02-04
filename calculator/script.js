dbd686ee9e69b5cb57b995fe21a8ae0665b5cc4e
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-02-03 21:05:47 -0700

    Add basic functionality and layout

---
+++ calculator/script.js
@@ -0,0 +1,32 @@
+let leftNumber = "";
+let rightNumber = "";
+let operand = "";
+const operators = ['+', '-', '*', '/'];
+
+const add = (num1, num2) => (num1 + num2);
+const substract = (num1, num2) => (num1 - num2);
+const multiply = (num1, num2) => (num1 * num2);
+const divide = (num1, num2) => (num1 / num2);
+
+const operate = (op, num1, num2) => {
+    switch (op) {
+        case '+':
+            return add(num1,num2);
+            break;
+        case '-':
+            return substract(num1,num2);
+            break;
+        case '*':
+            return multiply(num1,num2);
+            break;
+        case '/':
+            return divide(num1,num2);
+            break;
+    
+        default:
+            return "Syntax Error";
+            break;
+    }
+}
+
+console.log(operate('-', 1, 3));
