1cadc08f3ce0b6209c4a627bf22211a9b9dcffa5
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-02-07 16:30:14 -0700

    add functionality but cluttered code

--- library/script.js
+++ library/script.js
@@ -6,45 +6,70 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.changeReadStatus = function() {
+    if (this.read === "read") {
+        this.read = "not read";
+    } else {
+        this.read = "read";
+    };
+}
+
 function addBookToLibrary(title, author, pages, read) {
     const new_book = new Book(title, author, pages, read);
     myLibrary.push(new_book);
 }
 
-addBookToLibrary("El principito", "some", "123", "have read");
-addBookToLibrary("Harry Potter", "someone", "153", "have not read");
-addBookToLibrary("Crepusculo", "someoneone", "323", "have read");
+addBookToLibrary("El principito", "some", "123", "read");
+addBookToLibrary("Harry Potter", "someone", "153", "not read");
+addBookToLibrary("Crepusculo", "someoneone", "323", "read");
 
 function displayLibrary() {
+    const container = document.querySelector("div.container");
     container.textContent = "";
-    for (book of myLibrary) {
-        const book_div = document.createElement("div");
+    for (let [index,book] of myLibrary.entries()) {
+        const bookDiv = document.createElement("div");
         const title = document.createElement("div");
         const author = document.createElement("div");
         const pages = document.createElement("div");
         const read = document.createElement("div");
+        const deleteButton = document.createElement("button");
+        const readStatus = document.createElement("button");
         title.textContent = book.title;
         author.textContent = book.author;
         pages.textContent = book.pages;
         read.textContent = book.read;
-        book_div.appendChild(title);
-        book_div.appendChild(author);
-        book_div.appendChild(pages);
-        book_div.appendChild(read);
-        container.appendChild(book_div);
+        deleteButton.textContent = "Delete";
+        readStatus.textContent = "Have read?";
+        bookDiv.appendChild(title);
+        bookDiv.appendChild(author);
+        bookDiv.appendChild(pages);
+        bookDiv.appendChild(read);
+        bookDiv.appendChild(deleteButton);
+        bookDiv.appendChild(readStatus);
+        bookDiv.setAttribute("data-id", index);
+        deleteButton.addEventListener("click", () => {
+            myLibrary.splice(bookDiv.dataset.id, 1);
+            bookDiv.remove();
+            //console.table(myLibrary);
+            displayLibrary();
+        });
+        readStatus.addEventListener("click", () => {
+            book.changeReadStatus();
+            displayLibrary();
+        });
+        container.appendChild(bookDiv);
     }
 }
 
-const container = document.querySelector("div.container");
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("button.cancel");
 
-const book_title = document.querySelector('#book-title');
-const book_author = document.querySelector('#book-author');
-const book_pages = document.querySelector('#book-pages');
-const book_have_read = document.querySelector('#book-have-read');
+const bookTitle = document.querySelector('#book-title');
+const bookAuthor = document.querySelector('#book-author');
+const bookPages = document.querySelector('#book-pages');
+const bookHaveRead = document.querySelector('#book-have-read');
 const acceptButton = document.querySelector("input.accept");
 
 showButton.addEventListener("click", () => {
@@ -57,8 +82,12 @@ closeButton.addEventListener("click", () => {
 
 acceptButton.addEventListener("click", (event) => {
     event.preventDefault();
-    addBookToLibrary(book_title.value, book_author.value, book_pages.value, book_have_read.value);
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookHaveRead.value);
     dialog.close()
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    bookHaveRead.value = "";
     displayLibrary();
 });
 
