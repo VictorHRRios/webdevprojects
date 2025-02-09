1a90bcb934b1b2ae4b9fc7f04e8cbb168524180b
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-02-09 10:52:50 -0700

    Refactor code and simplicity

--- library/script.js
+++ library/script.js
@@ -1,13 +1,14 @@
 const myLibrary = [];
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
 
-Book.prototype.changeReadStatus = function() {
-    this.read ? this.read = false : this.read = true; 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    changeReadStatus = () => this.read ? this.read = false : this.read = true;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -15,81 +16,87 @@ function addBookToLibrary(title, author, pages, read) {
     displayLibrary();
 }
 
-
 addBookToLibrary("El principito", "some", "123", "read");
 addBookToLibrary("Harry Potter", "someone", "153", "not read");
 addBookToLibrary("Crepusculo", "someoneone", "323", "read");
 
 function displayLibrary() {
     const container = document.querySelector("div.container");
-    container.textContent = "";
-    for (let [index,book] of myLibrary.entries()) {
-        const bookDiv = document.createElement("div");
-        const title = document.createElement("div");
-        const author = document.createElement("div");
-        const pages = document.createElement("div");
-        const read = document.createElement("div");
-        const deleteButton = document.createElement("button");
-        const readStatus = document.createElement("button");
-        const divButtons = document.createElement("div");
+    container.textContent = '';
 
-        title.textContent = `Title: ${book.title}`;
-        author.textContent = `Author: ${book.author}`;
-        pages.textContent = `Pages: ${book.pages}`;
-        read.textContent = `${book.read ? "Read" : "Not Read"}`;
-        deleteButton.textContent = "Delete";
-        readStatus.textContent = "Have read?";
-
-        bookDiv.appendChild(title);
-        bookDiv.appendChild(author);
-        bookDiv.appendChild(pages);
-        bookDiv.appendChild(read);
-        divButtons.appendChild(deleteButton);
-        divButtons.appendChild(readStatus);
-        bookDiv.appendChild(divButtons);
-        bookDiv.setAttribute("data-id", index);
-        bookDiv.classList.add("book");
-
-        deleteButton.addEventListener("click", () => {
-            myLibrary.splice(bookDiv.dataset.id, 1);
-            bookDiv.remove();
-            displayLibrary();
-        });
-        readStatus.addEventListener("click", () => {
-            book.changeReadStatus();
-            read.textContent = book.read ? "Read" : "Not Read";
-        });
+    myLibrary.forEach((book, index) => {
+        const bookDiv = createBookElement(book, index);
         container.appendChild(bookDiv);
-    }
+    });
+}
+
+function createBookElement(book, index) {
+    const bookDiv = document.createElement("div");
+    bookDiv.setAttribute("data-id", index);
+    bookDiv.classList.add("book");
+
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pages = document.createElement("div");
+    const read = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    const readStatus = document.createElement("button");
+    const divButtons = document.createElement("div");
+
+    title.textContent = `Title: ${book.title}`;
+    author.textContent = `Author: ${book.author}`;
+    pages.textContent = `Pages: ${book.pages}`;
+    read.textContent = `${book.read ? "Read" : "Not Read"}`;
+    deleteButton.textContent = "Delete";
+    readStatus.textContent = "Have read?";
+
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(pages);
+    bookDiv.appendChild(read);
+    divButtons.appendChild(deleteButton);
+    divButtons.appendChild(readStatus);
+    bookDiv.appendChild(divButtons);
+
+    deleteButton.addEventListener("click", () => {
+        myLibrary.splice(bookDiv.dataset.id, 1);
+        bookDiv.remove();
+        displayLibrary();
+    });
+    readStatus.addEventListener("click", () => {
+        book.changeReadStatus();
+        read.textContent = book.read ? "Read" : "Not Read";
+    });
+
+    return bookDiv;
 }
 
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".dialog-button > button");
+const form = document.querySelector('form');
 const closeButton = document.querySelector("button.cancel");
-
-const bookTitle = document.querySelector('#book-title');
-const bookAuthor = document.querySelector('#book-author');
-const bookPages = document.querySelector('#book-pages');
-const bookHaveRead = document.querySelector('#book-have-read');
-const acceptButton = document.querySelector("input.accept");
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener("click", (event) => {
+    event.preventDefault();
     dialog.close();
 });
 
-acceptButton.addEventListener("click", (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookHaveRead.value);
+    const formData = new FormData(event.target);
+    const bookTitle = formData.get('book-title');
+    const bookAuthor = formData.get('book-author');
+    const bookPages = formData.get('book-pages');
+    const bookHaveRead = formData.get('book-have-read');
+    console.table(formData);
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookHaveRead);
+    form.reset()
     dialog.close()
-    bookTitle.value = "";
-    bookAuthor.value = "";
-    bookPages.value = "";
-    bookHaveRead.value = "";
 });
 
 displayLibrary();
